22eb99746a360e49daa870451e78887d144109b8
Kyohei Yamaguchi <kyohei3235@gmail.com>
2018-09-22 17:47:42 +0900

    My first commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Kyohei Yamaguchi on 2018/09/22.
+//  Copyright © 2018年 Kyohei Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
