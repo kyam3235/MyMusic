092a377483ffea83937e36052570ed04d6b186f3
Kyohei Yamaguchi <kyohei3235@gmail.com>
2018-09-24 21:16:43 +0900

    サウンドプレーヤーの処理を関数にまとめた

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -10,71 +10,49 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    //シンバルの音源ファイルを指定
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    //ギターの音源ファイルを指定
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    //バックミュージックの音源ファイルを指定
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    //シンバル用のプレーヤーインスタンスを作成
+    var cymbalPlayer = AVAudioPlayer()
+    //ギター用のプレ-ヤーインスタンスを作成
+    var guitarPlayer = AVAudioPlayer()
+    //バックミュージック用のプレーヤーインスタンを作成
+    var backmusicPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    //シンバルの音源ファイルを指定
-    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
-    
-    //シンバル用のプレーヤーインスタンスを作成
-    var cymbalPlayer = AVAudioPlayer()
-    
     @IBAction func cymbal(_ sender: Any) {
-        do{
-            //シンバル用のプレイヤーに、音源ファイル名を指定
-            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
-            
-            //シンバルの音源再生
-            cymbalPlayer.play()
-        }catch{
-            print("シンバルでエラーが発生しました")
-        }
+        soundPlayer(player: &cymbalPlayer, path: cymbalPath, count: 0)
     }
-    
-    //ギターの音源ファイルを指定
-    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
-
-    //ギター用のプレ-ヤーインスタンスを作成
-    var guitarPlayer = AVAudioPlayer()
     
     @IBAction func guitar(_ sender: Any) {
-        do{
-            //ギター用のプレイヤーに、音源ファイル名を指定
-            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
-            
-            //ギターの音源再生
-            guitarPlayer.play()
-        }catch{
-            print("ギターでエラーが発生しました")
-        }
+        soundPlayer(player: &guitarPlayer, path: guitarPath, count: 0)
     }
     
-    //バックミュージックの音源ファイルを指定
-    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
-    
-    //バックミュージック用のプレーヤーインスタンを作成
-    var backmusicPlayer = AVAudioPlayer()
-    
     @IBAction func play(_ sender: Any) {
-        do{
-            //バックミュージック用のプレイヤーに、音源ファイル名を指定
-            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
-            
-            //リピート設定
-            backmusicPlayer.numberOfLoops = -1
-            //バックミュージックの音源再生
-            backmusicPlayer.play()
-        }catch{
-            print("エラーが発生しました")
-        }
+        soundPlayer(player: &backmusicPlayer, path: backmusicPath, count: -1)
     }
     
     @IBAction func stop(_ sender: Any) {
         //バックミュージックの停止
         backmusicPlayer.stop()
+    }
+    
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path:URL, count:Int){
+        do{
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        }catch{
+            print("エラーが発生しました")
+        }
     }
 }
 
