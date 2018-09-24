6033c792992a0051bff96577937894894de8cea1
Kyohei Yamaguchi <kyohei3235@gmail.com>
2018-09-24 16:57:56 +0900

    シンバルボタンをタップしたらシンバル音源を再生するようにした

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -16,6 +16,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    //シンバルの音源ファイルを指定
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    
+    //シンバル用のプレーヤーインスタンスを作成
+    var cymbalPlayer = AVAudioPlayer()
+    
+    @IBAction func cymbal(_ sender: Any) {
+        do{
+            //シンバル用のプレイヤーに、音源ファイル名を指定
+            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+            
+            //シンバルの音源再生
+            cymbalPlayer.play()
+        }catch{
+            print("シンバルでエラーが発生しました")
+        }
+    }
 }
 
